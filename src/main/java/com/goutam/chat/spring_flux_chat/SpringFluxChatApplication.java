aa5fe7d2bb942a905f3280af7548a5eab8ac8d7a
goutamkumar120 <goutamkumar120@gmail.com>
2025-09-24 20:41:36 +0530

    First commit - project setup

---
+++ src/main/java/com/goutam/chat/spring_flux_chat/SpringFluxChatApplication.java
@@ -0,0 +1,13 @@
+package com.goutam.chat.spring_flux_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFluxChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFluxChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goutam/chat/spring_flux_chat/SpringFluxChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goutam.chat.spring_flux_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFluxChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
